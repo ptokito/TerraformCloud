b0632555a34847a6e26b4d029f1a913771b2f03b
Tim Okito <62076924+ptokito@users.noreply.github.com>
2020-11-17 13:26:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,4 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "myec2" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-
-  tags =  {
-    Name = "web server"
-  }
-  depends_on = [aws_instance.myec2db]
+    ami = "ami-0947d2ba12ee1ff75"
+    instance_type = "t2.micro"
 }
-
-
-resource "aws_instance" "myec2db" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "db server"
-  }
-}
-
-
-
-/* done 
-*/ 
